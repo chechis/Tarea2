97c2baf27ea7c31a871f36d9a9401d63d7d6d69a
chechis <jcalvarezg92@gmail.com>
2019-10-21 14:15:54 -0600

    longitud altura

--- Tarea2.m
+++ Tarea2.m
@@ -6,22 +6,36 @@ clc
 elasticidad = input('Ingrese el modulo de eslasticidad: ');
 area = input('Ingrese el area: ');
 longitud = input('Ingrese la longitud: ');
-tramos = input('Ingrese la cantidad de tramos: ');
 altura = input('Ingrese la altura del puente: ');
+tramos = input('Ingrese la cantidad de tramos: ');
 
 %% grados de libertad globales
 if(tramos ==0)
-    n = 4
-elseif(tramos==1)
-    
-elseif(tramos>1)
-    
+    n = 4;
+elseif(tramos>0)
+    n = 4+2*tramos;
 end
+
 for i=1:n
    gdl(i,:) = [2*i-1 2*i]; 
 end
+%% coordenadas del sistema
 
+xy= zeros(n, 2);
 
+m= n/2+2;
+
+for i=2:m
+    xy(i,1)= longitud*(i-1) 
+end
+
+for j= m:n
+    xy(j,1)= longitud*()
+end
+
+for j= m:n
+    xy(j,2)= altura
+end
 
 
 
