8b223e211908b254a2029da070515b37abfe197d
chechis <jcalvarezg92@gmail.com>
2019-10-21 13:03:20 -0600

    gdl

--- Tarea2.m
+++ Tarea2.m
@@ -2,6 +2,28 @@ close all
 clear all
 clc
 
-E = input('Ingrese el modulo de eslasticidad: ');
-A = input('Ingrese el area: ');
+%% ingresando datos
+elasticidad = input('Ingrese el modulo de eslasticidad: ');
+area = input('Ingrese el area: ');
+longitud = input('Ingrese la longitud: ');
+tramos = input('Ingrese la cantidad de tramos: ');
+altura = input('Ingrese la altura del puente: ');
+
+%% grados de libertad globales
+if(tramos ==0)
+    n = 4
+elseif(tramos==1)
+    
+elseif(tramos>1)
+    
+end
+for i=1:n
+   gdl(i,:) = [2*i-1 2*i]; 
+end
+
+
+
+
+
+
 
